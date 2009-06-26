f4885eceebc47190c85f6dd04f567237571464f2
Alexander Meinke <ameinke@online.de>
2009-06-26 06:12:07 +0200

    stdio.mac is now implemented as functions. function.asm is the program to test new functions. a new string.mac is in progress.

--- function.asm
+++ function.asm
@@ -8,10 +8,19 @@
 ; VERSION:		0.1
 ;-----------------------------------------------------
 
-CPU 186
+CPU 8086
 
 SEGMENT DATA USE16
 	msg:	db		"Hello World!$"
+	char:	db		0x00
+	input:	db		11
+			resb	1
+			times	11 db 0
+	length:	times	10 db 0
+	ascii:	db		11
+			resb	1
+			times	11 db 0
+	lint:	dd 		0
 
 
 SEGMENT STACK STACK USE16
@@ -19,16 +28,64 @@ SEGMENT STACK STACK USE16
 
 
 SEGMENT CODE USE16
+	%include "stdio.mac"
+	%include "new_string.mac"
+
 ..start:
-	%include "new_stdio.mac"
+
 	; INIT SEGMENTS
 	mov ax, DATA
 	mov ds, ax
 	mov es, ax
+	
 
 	; DOS - WRITE STRING
-	mov dx, msg
-	call print
+	mov ax, msg
+	push ax
+	call printnl
+	pop ax
+
+	mov ax, input
+	push ax
+	call scan
+	pop ax
+
+	call newline
+
+	mov bx, input
+	add bx, 2
+	push ax
+	push dx
+	push bx
+	call atoi
+	pop bx
+	pop dx
+	pop ax
+
+	mov [lint], ax
+	mov [lint+2], dx
+
+;	push ax
+;	call putc
+;	pop ax
+
+	push ax
+	push dx
+	mov bx, ascii
+	add bx, 2
+	push bx
+	call itoa
+	pop bx
+	pop dx
+	pop ax
+
+	mov bx, ascii
+	add bx, 2
+	push bx
+	call printnl
+	pop bx
+
+
 
 	; DOS - RETURN
 exit:
